a595f2db61806908ed0f8ac3354f90bfff4fd000
root <bitsha256@gmail.com>
2015-07-10 20:51:04 +0800

    the chain id was wrong

--- libraries/blockchain/include/bts/blockchain/fork_blocks.hpp
+++ libraries/blockchain/include/bts/blockchain/fork_blocks.hpp
@@ -8,7 +8,7 @@
 #include <vector>
 
 #define PLS_EXPECTED_CHAIN_ID       digest_type( "85d334301fea511328af381349e357e4c5b628859af98de08fbb8639262c29f4" )
-#define PLS_DESIRED_CHAIN_ID        digest_type( "1b7e602523026b6a51acb884d055e04ab4e89ab864060a63bcf6f664df2fcb19" )
+#define PLS_DESIRED_CHAIN_ID        digest_type( "bbf8cbb90532eb555f66602d3bf071609552f852cf9156d4253a33479b70a5e1" )
 
 #define PLS_V0_0_3_FORK_BLOCK_NUM     4000
 #define PLS_V0_1_0_FORK_BLOCK_NUM   574000
